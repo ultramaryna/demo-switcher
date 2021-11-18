cbbf0abcfd12022bef1ae388321851a7b92a0c80
Julia Kwiecien <julia.kwiecien@hatimeria.com>
2021-11-18 12:16:36 +0100

    Init default switcher in preview

--- public/init.js
+++ public/init.js
@@ -1,4 +1,4 @@
 (function() {
   const switcher = new window.HatimeriaDemoSwitcher();
-  switcher.render({ target: '#demo-switcher', type: 'homepage' });
+  switcher.render({ target: '#demo-switcher' });
 })();
